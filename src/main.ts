a4237902a3afbd54025217e872a00de1650248e0
TSantoro <theoalmeida00@gmail.com>
2025-03-31 02:15:04 -0300

    changed url to new domain

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    // Habilita CORS se seu front estiver em domínio diferente
-    app.enableCors();
+
+    // Configuração específica de CORS
+    app.enableCors({
+        origin: ['https://master.d27eestsfa8318.amplifyapp.com', 'http://localhost:3000'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+        allowedHeaders: 'Content-Type, Accept',
+    });
+
     const port = process.env.PORT || 8080;
     await app.listen(port);
     console.log(`Aplicação rodando em: ${await app.getUrl()}`);
 }
-
 bootstrap();
